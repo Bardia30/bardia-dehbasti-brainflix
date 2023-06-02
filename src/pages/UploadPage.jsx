bd93d0508a881d10087d0a7edafeaed2e098e085
Bardia Dehbasti <bardiadehbasti@gmail.com>
2023-06-02 19:09:00 -0400

    added components, styling and components separation left to do

--- src/pages/UploadPage.jsx
+++ src/pages/UploadPage.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from 'react';
+import Upload from '../components/HeaderComponents/Upload';
+import publishIcon from '../assets/Icons/publish.svg';
 
-function Upload() {
+function UploadPage() {
   return (
-    <div className='upload'>
-        <h1>This is the Upload Page</h1>
-    </div>
+    <section className='upload-page'>
+        <h1 className='upload-page__title'>Upload Video</h1>
+        <div className='upload-section'>
+          <section className='upload-section__left'>
+            <h3 className='upload-section__title'>video thumbnail</h3>
+            <img src="" alt="" />
+          </section>
+          <form className='upload-form'>
+            <label className='upload-form__label' htmlFor="videoTitle">title your video</label>
+            <input name='videoTitle' type="text" placeholder='Add a title to your video'/>
+            <label className='upload-form__label' htmlFor="videoDescription">Add a video description</label>
+            <input type="text" name="videoDescription" placeholder='Add a description to your video'/>
+          </form>
+        </div>
+        <div className='buttons-section'>
+          <button className='buttons-section__cancel-cta'>cancel</button>
+          <Upload
+            classUpload="navbar__upload-section" 
+            classImg="navbar__upload-icon" 
+            classText="navbar__upload-text"
+            icon={publishIcon} 
+            alt="publish-icon" 
+            text="publish"
+          />
+        </div>
+    </section>
   )
 }
 
-export default Upload
+export default UploadPage;
