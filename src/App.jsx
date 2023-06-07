e8023dae3ba23e21731c20d021b15b27aeb889ab
Bardia Dehbasti <bardiadehbasti@gmail.com>
2023-06-06 22:16:24 -0400

    fixed routings

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/HeaderComponents/Header';
 import HomePage from './pages/HomePage';
-import VideoDetailsPage from './pages/VideoDetailsPage';
 import UploadPage from './pages/UploadPage';
 
 
@@ -15,7 +14,7 @@ function App() {
         <Header/>
         <Routes>
           <Route path='/' element={<HomePage />} />
-          <Route path='/Home/VideoDetailsPage/:videoId' element={<VideoDetailsPage />} />
+          <Route path='/:videoId' element={<HomePage />} />
           <Route path='/upload' element={<UploadPage />} />
         </Routes>
       </BrowserRouter>
