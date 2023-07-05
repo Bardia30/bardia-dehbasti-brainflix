aa5243a29bd3c59f0712659d37f9ded0b66112cc
Bardia Dehbasti <bardiadehbasti@gmail.com>
2023-07-05 19:45:51 -0400

    fixed the HomePage axios useEffect logic to check for the first videos id upon page relaod, instead of hardcoding it

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -14,33 +14,40 @@ function HomePage() {
     // const api_key = '?api_key=f5296aef-42b9-41cb-a604-bf1e079f7f17'
 
     //a state that hold the id of the current id, to show on video player, the first one as default
-    const [currentVideoId, setCurrentVideoId] = useState("84e96018-4022-434e-80bf-000ce4cd12b8"); //state that holds the id of the selected video
+    const [currentVideoId, setCurrentVideoId] = useState(); //state that holds the id of the selected video
     
     const params = useParams();
     const { videoId } = params;
+
+
+    
 
     //to get the videosData (for articles)
     useEffect(() => {
         axios.get(APIUrlVideos)
             .then((res) => {
                 setVideosData(res.data);
+                setCurrentVideoId(res.data[0].id);
                 //checks if dynamic url parameter (video ID) exists, or when user has clicked on a video in next videos, then sets the currentVideoId state to the selected video 
                 if (videoId) {
                     setCurrentVideoId(videoId);
                 }
                 //this checks if videoId is undefined, to set the currentVideoId to the default one, mainly was created for when user clicks on the logo, to bring to homepage with default video  
                 if (videoId === undefined) {
-                    setCurrentVideoId("84e96018-4022-434e-80bf-000ce4cd12b8")
+                    setCurrentVideoId(res.data[0].id);
                 }
                 return res.data;
             })
             //if another video has been clicked, do an axios call to get the detail video data 
-            .then(()=> {
-                axios.get(`${APIUrlVideos}/${currentVideoId}`)
+            .then((res)=> {
+                if (currentVideoId) {
+                    axios.get(`${APIUrlVideos}/${currentVideoId}`)
                     .then(res => {
                         setVideoDetailsData(res.data);
                     })
                     .catch(err => console.log(err.message))
+                }
+                
             })
             .catch(err => console.log(err.message))
 
